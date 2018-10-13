0fcacbece9a738952abe37355f2615fa12d6ab76
TardigradeS2 <dothisnows@gmail.com>
2018-10-13 21:10:14 +0700

    User can upload image to use as their avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  mount_uploader :image, ImageUploader 
+
+  # User Avatar Validation
+  validates_integrity_of  :image
+  validates_processing_of :image
 end
