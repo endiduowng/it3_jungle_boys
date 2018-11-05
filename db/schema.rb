ed6899a8fe25f7e1b1c2212ab3914e8d290a2cfa
nguyenvanduong20140887 <nguyenvanduong.vn.k59@gmail.com>
2018-11-05 19:01:31 +0700

    lastest-anime-review

--- db/schema.rb
+++ db/schema.rb
@@ -40,46 +40,6 @@ ActiveRecord::Schema.define(version: 20181101152710) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "average_caches", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "rater_id"
-    t.string "rateable_type"
-    t.integer "rateable_id"
-    t.float "avg", limit: 24, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "overall_averages", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "rateable_type"
-    t.integer "rateable_id"
-    t.float "overall_avg", limit: 24, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rates", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "rater_id"
-    t.string "rateable_type"
-    t.integer "rateable_id"
-    t.float "stars", limit: 24, null: false
-    t.string "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
-    t.index ["rater_id"], name: "index_rates_on_rater_id"
-  end
-
-  create_table "rating_caches", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "cacheable_type"
-    t.integer "cacheable_id"
-    t.float "avg", limit: 24, null: false
-    t.integer "qty", null: false
-    t.string "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
-  end
-
   create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "review_score"
     t.string "review_description"
