718442f2c912c934ae13065645ac3ee6971cb571
hienpdbk <hpd.hienpham@gmail.com>
2018-11-06 12:19:51 +0700

    done top airing, upcoming animes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @saved = current_user.adds
+    @top_airing_animes   = Anime.select_top_airing
+    @top_upcoming_animes = Anime.select_top_upcoming
   end
 end
--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,3 +1,4 @@
+require "time"
 class Anime < ApplicationRecord
   has_many :adds
   has_many :reviews, -> {order(:created_at => :desc)}
@@ -6,5 +7,17 @@ class Anime < ApplicationRecord
     if user
       Add.find_by(user_id: user.id, anime_id: id)
     end
+  end
+
+  def self.select_top_airing
+    Anime.
+      where('premiered < ?', Date.today).
+      order(score: :desc)
+  end
+
+  def self.select_top_upcoming
+    Anime.
+      where('premiered > ?', Date.today).
+      order(score: :desc)
   end
 end
