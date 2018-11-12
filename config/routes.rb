1ef873684d86ade5092157fe28c84087fce21325
TardigradeS2 <dothisnows@gmail.com>
2018-11-12 22:50:04 +0700

    User can select anime according to season

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
   post '/search', to: 'static_pages#result'
   get '/search', to: 'static_pages#search'
 
-  get "/anime_list_view_more", to: "animes#anime_list"
+  get '/season', to: "animes#anime_list"
+
   get "/anime_airing_rank_list_view_more", to: "animes#anime_airing_rank_list"
   get "/anime_upcoming_rank_list_view_more", to: "animes#anime_upcoming_rank_list"
 end
